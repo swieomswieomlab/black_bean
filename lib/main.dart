84f7832402c268811d73cb975d079458cf44adf2
Yechan Jung <asdxcv1998@gmail.com>
2023-03-12 19:42:22 +0900

    page folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
-import 'package:black_bean/test_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
-import 'home_page.dart';
+import 'pages/home_page.dart';
 import 'firebase_options.dart';
+import 'pages/test_page.dart';
 
 void main() async {
   await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-    );
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
