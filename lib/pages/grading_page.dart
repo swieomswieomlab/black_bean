fd79e3ee1d6970a46ed5c2ed8c10f70161a9cc6c
Cho <choyc4643@naver.com>
2023-04-19 01:04:27 +0900

    Grading Page 백 연결 전 완성

--- lib/pages/grading_page.dart
+++ lib/pages/grading_page.dart
@@ -1,41 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:black_bean/textstyle.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../model/problem.dart';
 
 class GradingPage extends StatefulWidget {
-  const GradingPage({super.key});
+  const GradingPage({Key? key}) : super(key: key);
 
   @override
-  State<GradingPage> createState() => _GradingPageState();
+  _GradingPageState createState() => _GradingPageState();
 }
 
 class _GradingPageState extends State<GradingPage> {
-//시험 결과 이 변수에 저장 필요
+  //시험 결과 이 변수에 저장 필요
   List<int> Plist = List.generate(20, (index) => 0);
 
   List<Widget> textWidgets = [];
   bool clicked = false;
+  bool isHovered = false;
+
 
   @override
   void initState() {
-    //시험 결과 내용을 Plist에서 받아와서 textWidgets에 저장
-    textWidgets = Plist.map<Widget>((int number) {
-      return InkWell(
+textWidgets = Plist.map<Widget>((int number) {
+  bool _isHovered = false;
+  return MouseRegion(
+    onHover: (event) {
+      setState(() {
+        _isHovered = true;
+      });
+    },
+    onExit: (event) {
+      setState(() {
+        _isHovered = false;
+      });
+    },
+    child: InkWell(
+      onTap: () {
+        // do something when the widget is tapped
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
         child: Container(
-            width: 70,
-            height: 94,
-            color: mainLightBlue,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(number.toString()),
-                Text("\u{274c}"),
-              ],
-            )),
-      );
-    }).toList();
+          width: 70,
+          height: 94,
+          color: _isHovered ? Colors.red : mainLightBlue,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(number.toString()),
+              Text("\u{274c}"),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}).toList();
+
+
     super.initState();
   }
 
@@ -50,17 +73,21 @@ class _GradingPageState extends State<GradingPage> {
         });
       },
       style: ButtonStyle(
-        side: MaterialStateProperty.all(BorderSide(color: Colors.black)),
-        backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-          (states) {
-            if (isSelected) {
-              return Colors.blue;
-            } else {
-              return null;
-            }
-          },
-        ),
-      ),
+  shape: MaterialStateProperty.all(CircleBorder()),
+  side: MaterialStateProperty.all(BorderSide(color: Colors.black)),
+  backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+    (states) {
+      if (isSelected) {
+        return Colors.blue;
+      } else {
+        return null;
+      }
+    },
+  ),
+      fixedSize: MaterialStateProperty.all(Size(40, 40)),
+
+),
+
       child: Text(
         number,
         style: TextStyle(
@@ -81,11 +108,12 @@ class _GradingPageState extends State<GradingPage> {
         title: Row(
           children: [
             SizedBox(
-                width: 100,
-                child: Image.network(
-                  'https://gradium.co.kr/wp-content/uploads/black-beans-1.jpg',
-                  fit: BoxFit.contain,
-                )),
+              width: 100,
+              child: Image.network(
+                'https://gradium.co.kr/wp-content/uploads/black-beans-1.jpg',
+                fit: BoxFit.contain,
+              ),
+            ),
             SizedBox(
               width: 80,
             ),
@@ -108,114 +136,162 @@ class _GradingPageState extends State<GradingPage> {
             ),
           ],
         ),
-        actions: [TextButton(onPressed: () {}, child: Text("마이페이지"))],
+        actions: [TextButton(onPressed: () {}, child: Text("마이페이지", style: Button_Bt2(20, Colors.black)),)],
       ),
-      body: SingleChildScrollView(
+      body: 
+
+           Center(
+            child: SingleChildScrollView(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Container(
+                  width: 1200,
+                  // height: screenHeight,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 1200,
+                        height: 178,
+                        // margin: EdgeInsets.symmetric(
+                        //   horizontal: 156,
+                        // ),
+                        color: Color(0xffF3F8FC),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "65점",
+                              style: Headline_H0(72, mainSkyBlue),
+                            ),
+                            Text(
+                              "합격까지 한 문제! 너무 잘 하고 있어요 :)",
+                              style: Headline_H4(24, grey07),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Container(
+  height: 500,
+  child: Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Container(
+        width: 366,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              height: 178,
-              margin: EdgeInsets.symmetric(horizontal: 156.w),
-              color: Color(0xffF3F8FC),
-              // Color(0xFFF3F8FC),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      "65점",
-                      style: Headline_H0(72, mainSkyBlue),
-                    ),
-                    Text(
-                      "합격까지 한 문제! 너무 잘 하고 있어요 :)",
-                      style: Headline_H4(24, grey07),
-                    ),
-                  ]),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "문항 별 채점 결과",
+              style: Headline_H4(24, Colors.black),
             ),
             SizedBox(
-              height: 50,
+              height: 20,
             ),
             Container(
-              height: 500,
-              margin: EdgeInsets.symmetric(horizontal: 156),
-              child: Row(children: [
-                Container(
-                  width: 366,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("문항 별 채점 결과", style: Headline_H4(24, Colors.black)),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        width: 366,
-                        height: 388,
-                        child: GridView(
-                          shrinkWrap: true,
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 5,
-                            childAspectRatio: 70 / 94,
-                            mainAxisSpacing: 4.0,
-                            crossAxisSpacing: 4.0,
-                          ),
-                          children: textWidgets,
-                        ),
-                      )
-                    ],
-                  ),
+              width: 366,
+              child: GridView(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate:
+                    SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 5,
+                  childAspectRatio: 70 / 94,
+                  mainAxisSpacing: 4.0,
+                  crossAxisSpacing: 4.0,
                 ),
-                SizedBox(
-                  width: 60.w,
-                ),
-                VerticalDivider(
-                  width: 20,
-                  thickness: 1,
-                  indent: 20,
-                  endIndent: 0,
-                  color: Colors.grey,
-                ),
-                SizedBox(
-                  width: 60,
-                ),
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Text("틀린 문제 다시 풀기",
-                          style: Headline_H4(24, Colors.black)),
-                      SizedBox(height: 15,),
-                      SizedBox(
-                          child: Image.network(
-                        "https://t1.daumcdn.net/cfile/tistory/2267023D5464408A17",
-                        fit: BoxFit.contain,
-                      )),
-                      SizedBox(height: 130,),
-                      Container(
-
-                        child: Text("\u{270F}다시한번 풀어보세요",
-                            style: Headline_H4(24, Colors.black)),
-                      ),
-                      Row(
-                        children: [
-                          number_button('1', 1),
-                          number_button('2', 2),
-                          number_button('3', 3),
-                          number_button('4', 4),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ]),
+                children: textWidgets,
+              ),
             )
           ],
         ),
       ),
+      SizedBox(
+        width: 60,
+      ),
+      VerticalDivider(
+        width: 20,
+        thickness: 1,
+        indent: 20,
+        endIndent: 0,
+        color: Colors.grey,
+      ),
+      SizedBox(
+        width: 60,
+      ),
+      Expanded(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                "틀린 문제 다시 풀기",
+                style: Headline_H4(24, Colors.black),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 580,
+                child: Image.network(
+                  "https://t1.daumcdn.net/cfile/tistory/2267023D5464408A17",
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              Spacer(),
+              // SizedBox(
+              //   height: 80,
+              // ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 217,
+                  height: 38,
+                  color: Color(0xFFFFF5DA),
+                  child: Text(
+                    "\u{270F} 다시 한번 풀어보세요",
+                    style: Headline_H4(18, Colors.black),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Container(
+                width: 366,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    number_button('1', 1),
+                    number_button('2', 2),
+                    number_button('3', 3),
+                    number_button('4', 4),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+            ],
+          ),
+        ),
+      )
+    ],
+  ),
+)
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
     );
   }
 }
