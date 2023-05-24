174e196e3885131011a713b6c16a41f377cdb882
Cho <choyc4643@naver.com>
2023-05-24 10:14:34 +0900

    [UI] modify textStyles, colors

--- lib/textstyle.dart
+++ lib/textstyle.dart
@@ -4,6 +4,8 @@ const Color mainBlack = Color(0xff020c16);
 const Color mainLightBlue = Color(0xffeaf4fc);
 const Color maingreyblue = Color(0xffd5e5f1);
 const Color mainSkyBlue = Color(0xff43a7f5);
+const Color greyBlue = Color(0xffDEEBF4);
+
 const Color grey00 = Color(0xffffffff);
 const Color grey01 = Color(0xfff2f3f4);
 const Color grey02 = Color(0xffe6e7e8);
@@ -13,9 +15,39 @@ const Color grey05 = Color(0xff7e8385);
 const Color grey06 = Color(0xff64696a);
 const Color grey07 = Color(0xff414445);
 const Color grey08 = Color(0xff373838);
+const Color grey09 = Color(0xff020c16);
+
 const Color pointRed = Color(0xffeb4b4b);
 const Color pointYellow = Color(0xfff8cb74);
 const Color pointGreen = Color(0xff30d07a);
+
+const Color dimmed02 = Color(0x73000000);
+const Color dimmed01 = Color(0x40000000);
+
+const Color blue09 = Color(0xff43a7f5);
+const Color blue08 = Color(0xff4db0fd);
+const Color blue07 = Color(0xff6eb6ee);
+const Color blue06 = Color(0xffaacde7);
+const Color blue05 = Color(0xffc5e2f8);
+const Color blue04 = Color(0xffdcedfa);
+const Color blue03 = Color(0xffAACDE7);
+const Color blue02 = Color(0xffeaf4fc);
+const Color blue01 = Color(0xfff3f8fc);
+
+const Color green03 = Color(0xff27c06d);
+const Color green02 = Color(0xffe7fff2);
+const Color green01 = Color(0xfff3fff8);
+
+const Color red03 = Color(0xfff25d5d);
+const Color red02 = Color(0xfffde5e5);
+const Color red01 = Color(0xfffef5f5);
+
+const Color yellow06 = Color(0xffdec46a);
+const Color yellow05 = Color(0xfffcce2e);
+const Color yellow04 = Color(0xfffdda61);
+const Color yellow03 = Color(0xfffdf7e3);
+const Color yellow02 = Color(0xfffcfaf4);
+const Color yellow01 = Color(0xfffffefa);
 
 TextStyle Headline_H0(double fontsize, Color color) {
   return TextStyle(
@@ -199,5 +231,113 @@ TextStyle Tiny_T3(double fontsize, Color color) {
       fontWeight: FontWeight.w500,
       height: 20 / 12,
       letterSpacing: 0,
+      color: color);
+}
+
+TextStyle brand(Color color) {
+  return TextStyle(
+      fontSize: 48,
+      fontWeight: FontWeight.w900,
+      fontFamily: "Pretendard",
+      height: 72 / 48,
+      color: color);
+}
+
+TextStyle title1(Color color) {
+  return TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.w700,
+      fontFamily: "Pretendard",
+      height: 32 / 28,
+      color: color);
+}
+
+TextStyle title2(Color color) {
+  return TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      height: 30 / 22,
+      color: color);
+}
+
+TextStyle title3(Color color) {
+  return TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      height: 30 / 20,
+      color: color);
+}
+
+TextStyle body1(Color color) {
+  return TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Pretendard",
+      height: 30 / 20,
+      color: color);
+}
+
+TextStyle body2(Color color) {
+  return TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Pretendard",
+      height: 24 / 18,
+      color: color);
+}
+
+TextStyle body3(Color color) {
+  return TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      height: 20 / 16,
+      color: color);
+}
+
+TextStyle body4(Color color) {
+  return TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Pretendard",
+      height: 20 / 16,
+      color: color);
+}
+
+TextStyle body5(Color color) {
+  return TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Pretendard",
+      height: 20 / 14,
+      color: color);
+}
+
+TextStyle body6(Color color) {
+  return TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Pretendard",
+      height: 20 / 14,
+      color: color);
+}
+
+TextStyle button1(Color color) {
+  return TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      height: 20 / 20,
+      color: color);
+}
+
+TextStyle button2(Color color) {
+  return TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Pretendard",
+      height: 18 / 18,
       color: color);
 }
